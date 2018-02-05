046fad91ce2dffce2bab5554d052fcc9064c60d6
Ramon Henrique <rhpr@cin.ufpe.br>
2018-02-04 21:26:50 -0300

    Add library ELL dependency
    The folder should be in a parent directory from the source

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
+#include <config.h>
+#include <ell/ell.h>
 
 int main (int argc, char* argv[])
 {
-    return 0;
+	if (!l_main_init())
+		return EXIT_FAILURE;
+	l_log_set_stderr();
+	l_debug_enable("*");
+	l_info("Gate controller daemon version %s", VERSION);
+
+	l_main_run();
+
+	l_main_exit();
+	return 0;
 }
